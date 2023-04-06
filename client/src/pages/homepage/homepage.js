df91b075f4344385acbf65077e6e00bbb54bfe95
SLeBron <sophieclebron@gmail.com>
2023-04-05 21:10:32 -0600

    started homepage styling

---
+++ client/src/pages/homepage/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.css';
+
+function Homepage() {
+    return (
+        <div className="home">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
