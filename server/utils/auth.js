a66c94511d049140d64528509c89c5faf2da57f6
SLeBron <sophieclebron@gmail.com>
2023-04-09 14:13:16 -0600

    updated auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,3 +2,30 @@ const jwt = require("jsonwebtoken");
 
 const secret = "secret";
 const expiration = "1h";
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+        token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+        return req;
+    }
+
+    try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+    } catch {
+        console.log("Invalid token");
+    }
+
+    return req;
+    },
+    signToken: function ({ email, name, _id, registry }) {
+        const payload = { email, name, _id, registry };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
