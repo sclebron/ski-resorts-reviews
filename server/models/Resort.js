bca7966ba09945a8e193bb5c40f0aa48b93a6e27
SLeBron <sophieclebron@gmail.com>
2023-04-11 20:31:48 -0600

    updated resort model

--- server/models/Resort.js
+++ server/models/Resort.js
@@ -1,7 +1,21 @@
 const { Schema, model } = require('./mongoose');
 
 const resortSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    url: {
+        type: String,
+        required: true,
+        trim: true,
+    }
 })
 
 const Resort = model("Resort", resortSchema);
