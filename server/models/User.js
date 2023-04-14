efb2880c0326dc1e617051e2220ac93181d46283
SLeBron <sophieclebron@gmail.com>
2023-04-13 18:14:46 -0600

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,22 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     name: { 
         type: String, 
         required: true, 
         trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, "Must provide a valid email address!"],
     },
 })
 
