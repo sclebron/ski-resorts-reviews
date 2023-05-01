09b5cea08fd9df3de1b89de30011542d84f4b533
SLeBron <sophieclebron@gmail.com>
2023-04-30 23:51:26 -0600

    added route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Category from './pages/category/category.js';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          
+          <Route path='/Category' element={<Category/>}/>
         </Routes>
       </Router>
     </div>
